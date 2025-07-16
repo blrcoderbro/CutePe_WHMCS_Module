48cdc3f1dbaae4c7d645abdca5938cdb9094649a
Prithvi S <prithvisagar.56232@gmail.com>
2025-07-16 10:59:27 +0530

    Bug Fixes Implemented 1) Multiple OrderId Generation for SIngle Invoice Resolved 2) CallBack Issue Resolved

--- gateways/sql.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `tblpaymentgateways` ( `gateway`, `setting`, `value`, `order`) VALUES
-( 'cutepe', 'name', 'CutePe', 1),
-( 'cutepe', 'type', 'Payments', 0),
-('cutepe', 'visible', 'on', 0),
-('cutepe', 'cutepe_api_key', '', 0),
-('cutepe', 'merchant_key', '', 0),
-('cutepe', 'cutepe_module_auth_token', '', 0),
-('cutepe', 'convertto', '', 0);
